a3a70606fec4d34d695311c7f0a956cee2502906
Rahul Choudhary <rahul.choudhary.eee20@itbhu.ac.in>
2023-03-18 19:52:56 +0530

    Add rating feature

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose= require("mongoose");
 const passport= require("passport");
 const passportLocalMongoose= require("passport-local-mongoose");
 const session= require("express-session");
+const _= require("lodash");
 
 const app= express();
 
@@ -41,22 +42,117 @@ const courseSchema= new mongoose.Schema({
     url: String,
     creator: String,
     rating: Number,
-    ratedBy: Number
+    ratedBy: [String]
 })
 
 const Course= mongoose.model("Course",courseSchema);
+
+const fieldSchema= new mongoose.Schema({
+    fieldName: String,
+    upperField: String,
+    courses: [courseSchema],
+    imgURL: String
+});
+
+const Field= mongoose.model("Field", fieldSchema);
+
+// const newField= new Field({
+//     fieldName: "App Development",
+//     upperField: "tech",
+//     courses: [],
+//     imgURL: ""
+// })
+
+// newField.save();
+
+// const newFields= new Field({
+//     fieldName: "UPSC",
+//     upperField: "civilServices",
+//     courses: [],
+//     imgURL: ""
+// })
+
+// newFields.save();
 
 app.get("/", function(req,res){
     res.render("home");
 })
 
-app.get("/resources", function(req,res){
-    res.render("resources");
+app.get("/compose", function(req,res){
+    res.render("compose");
 })
 
-app.get("/resources/:courseField", function(req,res){
-    // console.log(req.params.courseField);
-    res.render("courseField", {courseField:req.params.courseField})
+app.post("/compose", async function(req,res){
+    // console.log(req.body);
+    try{
+        const field= await Field.findOne({fieldName: req.body.field}).exec();
+        // console.log(field);
+        const newCourse= new Course({
+            url: req.body.courseURL,
+            creator: req.body.creator,
+            rating: 0,
+            ratedBy: []
+        });
+
+        field.courses.push(newCourse);
+        field.save().then(function(){
+            res.redirect("/resources/"+req.body.field);
+        });
+    } catch(err){
+        console.log(err);
+    }
+})
+
+app.get("/resources",  async function(req,res){
+    try{
+        const fields= await Field.find();
+        res.render("resources", {fields:fields});
+    } catch(err){
+        console.log(err);
+    }
+});
+
+app.get("/resources/:courseField", async function(req,res){
+    try{
+        const field= await Field.findOne({fieldName: req.params.courseField});
+        res.render("courseField", {courseField:field, user: req.user});
+    } catch(err){
+        console.log(err);
+    }
+})
+
+app.post("/addRating", async function(req,res){
+    try{
+
+        const field= await Field.findById(req.body.courseField);
+
+        const course= field.courses.find(course => course.id===req.body.course);
+        const index= field.courses.indexOf(course => course.id===course);
+        course.ratedBy.push(req.body.user+req.body.rating);
+        course.rating= Math.round(((course.rating+Number(req.body.rating))/course.ratedBy.length)*10)/10;
+        await field.courses.splice(index,1,course);
+
+        if(field.courses.length>1){
+            await field.courses.sort((course1,course2) => { 
+                const a= course2.rating*course2.ratedBy.length;
+                const b= course1.rating*course1.ratedBy.length;
+                if(a<b){
+                    return -1;
+                }
+                if(a>b){
+                    return 1;
+                }
+                return 0;
+                // return course2.rating*course2.ratedBy.length - course1.rating*course1.ratedBy.length});
+            })
+        }
+        field.save().then(function(){
+            res.redirect("/resources/"+req.body.fieldName);
+        });
+
+    } catch(err){
+        console.log(err);
+    }
 })
 
 app.get("/mentors", function(req,res){
@@ -94,6 +190,16 @@ app.post("/register", function(req,res){
         }
     });
 })
+
+app.get("/logout", function(req,res){
+    req.logOut(function(err){
+        if(err){
+            console.log(err);
+        } else{
+            res.redirect("/");
+        }
+    });
+});
 
 app.listen(3000, function(){
     console.log("server is running on port 3000");
